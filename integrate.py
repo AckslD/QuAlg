969b877434ac09db7280b76bceabf3cee4bab14d
Axel Dahlberg <axel.dahlberg12@gmail.com>
2019-12-03 20:34:51 +0100

    Working on integration

---
+++ integrate.py
@@ -0,0 +1,148 @@
+from copy import copy
+from collections import namedtuple
+
+from toolbox import assert_str, replace_var
+from scalars import is_number, DeltaFunction, SumOfScalars, ProductOfScalars, InnerProductFunction,\
+    SingleVarFunctionScalar, Scalar, assert_is_scalar
+
+
+IntegrateResult = namedtuple("IntegrateResult", ["scalar", "applied"])
+
+
+class _Integration(Scalar):
+
+    def __init__(self, scalar, variable):
+        assert_is_scalar(scalar)
+        assert_str(variable)
+        if not isinstance(scalar, ProductOfScalars):
+            raise TypeError(f"scalar should be ProductOfScalars, not {type(scalar)}")
+        if not all(factor.has_variable(variable) for factor in scalar):
+            raise ValueError("all factors should have the integration term")
+        self._scalar = scalar
+        self._variable = variable
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        # TODO check with swapping the integration variable since this shouldn't matter
+        return self._scalar == other._scalar and self._variable == other._variable
+
+    def __str__(self):
+        return f"S_{self._variable}{{{self._scalar}}}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({repr(self._scalar)}, {repr(self._variable)}"
+
+    def __copy__(self):
+        return self.__class__(self._scalar, self._variable)
+
+    def conjugate(self):
+        return self.__class__(self._scalar.conjugate(), self._variable)
+
+    def simplify(self):
+        new_scalar = copy(self)
+        for evaluation in EVALUATIONS:
+            new_scalar = evaluation(new_scalar)
+            if not isinstance(new_scalar, _Integration):
+                return new_scalar
+        return new_scalar
+
+    def has_variable(self):
+        raise NotImplementedError()
+
+
+def integrate(scalar, variable):
+    assert_str(variable)
+    if isinstance(scalar, SumOfScalars):
+        # return
+        # results = []
+        # for s in scalar._terms:
+        #     result = integrate(s, variable)
+        # return [integrate(s, variable) for s in scalar._terms]
+        return sum(integrate(s, variable) for s in scalar._terms)
+    elif isinstance(scalar, ProductOfScalars):
+        # Split factors based on if they contain the integration variable or not
+        var_factors = []
+        other_factors = []
+        for factor in scalar._factors:
+            if isinstance(factor, Scalar) and factor.has_variable(variable):
+                var_factors.append(factor)
+            else:
+                other_factors.append(factor)
+        return ProductOfScalars(other_factors + [_Integration(ProductOfScalars(var_factors), variable)])
+        # for evaluate in EVALUATIONS:
+        #     result = evaluate(scalar, variable)
+        #     if result.applied:
+        #         return result
+        # return result
+        #     if out_scalar is not None:
+        #         return
+
+        # for facto
+        # var_factors = [factor for factor in scalar._factors if factor.has_variable(variable)]
+        # scalars, functions = scalar.split_scalars_and_functions()
+        # functions
+        # changed = True
+        # # import pdb
+        # # pdb.set_trace()
+        # while changed:
+        #     for evaluate in EVALUATIONS:
+        #         scalar, changed = evaluate(scalar, variable)
+        # return scalar
+    elif is_number(scalar):
+        return scalar
+    else:
+        raise NotImplementedError(f"integrate not implemented for type {type(scalar)}")
+
+
+def _evaluate_delta_function(integration_scalar):
+    integrand = integration_scalar._scalar
+    variable = integration_scalar._variable
+    # Find the delta functions containing this variable
+    deltas = [(i, d) for (i, d) in enumerate(integrand._factors)
+              if isinstance(d, DeltaFunction) and variable in d._vars]
+    if len(deltas) == 0:
+        return integration_scalar
+    elif len(deltas) > 2:
+        raise RuntimeError("Two delta functions with the same variable")
+    # Replace variables with the variable to the other variable in the delta function
+    i, delta = deltas[0]
+    # Get the other variable in the delta function
+    other_var = next(v for v in delta._vars if v != variable)
+    integrand = replace_var(integrand, old_variable=variable, new_variable=other_var)
+    # Remove the delta funtion (recall that replace_var creates a copy)
+    integrand._factors.pop(i)
+
+    return integrand
+
+
+def _find_norm_identities(integration_scalar):
+    """Finds integrals which are the norm of a function, i.e. 1"""
+    import pdb
+    pdb.set_trace()
+    integrand = integration_scalar._scalar
+    variable = integration_scalar._variable
+    # TODO generalise this
+    if len(integrand) == 2:
+        if all(isinstance(s, SingleVarFunctionScalar) for s in integrand):
+            if integrand[0] == integrand[1].conjugate():
+                return 1
+    return integration_scalar
+
+
+def _find_function_inner_products(integration_scalar):
+    """Finds integrals which evaluate to the inner product of functions."""
+    integrand = integration_scalar._scalar
+    variable = integration_scalar._variable
+    # TODO generalise this
+    if len(integrand) == 2:
+        if all(isinstance(s, SingleVarFunctionScalar) for s in integrand):
+            return InnerProductFunction(integrand[0]._func_name, integrand[1]._func_name)
+
+
+# These evaluations are used when integrating
+EVALUATIONS = [
+    _evaluate_delta_function,
+    _find_norm_identities,
+    _find_function_inner_products,
+]
